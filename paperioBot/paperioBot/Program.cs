d3c7d2181e39c84bbc76f8c3c42023a7062d2627
Askar Zaripov <askar@bashfin.com>
2019-08-08 11:06:49 +0500

    1

--- paperioBot/paperioBot/Program.cs
+++ paperioBot/paperioBot/Program.cs
@@ -2,6 +2,8 @@
 using paperioBot.Helpers;
 using paperioBot.InternalClasses;
 using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace paperioBot
 {
@@ -33,14 +35,28 @@ namespace paperioBot
 				} else if (step == 1)
 				{
 					_currentParams = JsonConvert.DeserializeObject<GameParams<WorldTickParams>>(input);
-					direction = DirectionHelper.FindFirstDirection(_currentParams.@params);
+					direction = DirectionHelper.Direction(0);
+					Console.WriteLine("{{\"command\": \"{0}\"}}", direction);
 				}
 				else
 				{
 					try
 					{
 						_currentParams = JsonConvert.DeserializeObject<GameParams<WorldTickParams>>(input);
-						direction = DirectionHelper.FindNewDirection(_currentParams.@params);
+
+						int nextDirection;
+						for (nextDirection = 0; nextDirection < 4; nextDirection++)
+						{
+							var me = _currentParams.@params.players["i"];
+							var nextPos = GetNextPos(me.position, nextDirection);
+							if (CheckIsInBound(nextPos) && !CheckInTail(nextPos, me.lines.ToArray()))
+							{
+								break;
+							}
+						}
+
+						direction = DirectionHelper.Direction(nextDirection);
+						Console.WriteLine("{{\"command\": \"{0}\"}}", direction);
 					}
 					catch (Exception)
 					{
@@ -52,9 +68,35 @@ namespace paperioBot
 				GameLogger.Log(direction);
 				GameLogger.SavePartialLogs();
 				
-				Console.WriteLine("{{\"command\": \"{0}\"}}", direction);
+				
 				step++;
 			}
+		}
+
+		private static int[] GetNextPos(int[] pos, int direction)
+		{
+			switch (direction)
+			{
+				case 0: return new[] {pos[0] - 30, pos[1]};
+				case 1: return new[] {pos[0] + 30, pos[1]};
+				case 2: return new[] {pos[0], pos[1] + 30};
+				case 3: return new[] {pos[0], pos[1] - 30};
+				default: throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		private static bool CheckIsInBound(int[] point)
+		{
+			const int cellSize = 30;
+			const int width = 930;
+			const int height = 930;
+			return point[0] >= cellSize / 2 && point[0] <= width - cellSize / 2 && point[1] >= cellSize / 2 &&
+			       point[1] <= height - cellSize / 2;
+		}
+
+		private static bool CheckInTail(int[] point, int[][] tail)
+		{
+			return tail.FirstOrDefault(x => x[0] == point[0] && x[1] == point[1]) != null;
 		}
 	}
 }
